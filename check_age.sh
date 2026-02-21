c5b3095aa497ad23713b67fa2796d56d26c911ae
ebrahimzayed123456789-max <ebrahimzayed123456789@gmail.com>
2026-02-21 16:46:57 +0200

    bash_2

---
+++ check_age.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 1. Create Required_Age variable
+Required_Age=27
+
+# 2. Prompt for username
+echo "Enter your username:"
+read NAME
+
+# 3. Prompt for age
+echo "Enter your age:"
+read AGE
+
+# Display entered data
+echo "Username: $NAME"
+echo "Age: $AGE"
+echo "Required Age is: $Required_Age"
